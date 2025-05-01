bcc8a287b093e0a90b315598737fa6dd42215168
Jamestrada <yeims007@gmail.com>
2025-05-01 14:13:05 -0500

    Schedule Tweets

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,11 @@ Rails.application.routes.draw do
   get "password/reset/edit", to: "password_resets#edit"
   patch "password/reset/edit", to: "password_resets#update"
 
+  get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
+
+  resources :twitter_accounts
+  resources :tweets
+
   root to: "main#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
